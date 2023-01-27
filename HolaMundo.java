a71bd4002026bc1eb5e4fdfbf1e38df1a5bc1d0e
SaSanti <sa.santillan93@gmail.com>
2023-01-26 23:59:44 -0300

    Actualizacion de un mensaje

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,17 +1,10 @@
-public class HolaMundo implements Educada{
+public class HolaMundo {
 	public void saludar(){
 		System.out.println("Hola Mundo!");
 	}
 	public static void main(String[] args){
-		Presentador p = new Presentador();
-		p.presentar();
-		
 		HolaMundo hm = new HolaMundo();
-		hm.saludar();
-		System.out.println("Este es mi primer clase en mi nuevo repositorio GIT :)");
-		System.out.println("Este si es el texto CORRECTO que se debe mostrar aqui!");
-		
-		Cerrador c = new Cerrador();
-		c.cerrar();
+		hm.saludar()
+		System.out.println("Cambio de mensaje desde la rama dev");
 	}
 }
